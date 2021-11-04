216d7820a481806ebc09d63cdcd83b8e158c7ff5
elwl43 <elwl43@gmail.com>
2021-11-04 16:24:09 +0900

    setTitle, setBody를 이용하여 게시물 수정 기능 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -55,6 +55,12 @@ public class UsrArticleController {
 		articles.remove(article);
 	}
 
+	private void modifyArticle(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		article.setTitle(title);
+		article.setBody(body);
+	}
 	// 서비스 메서드 끝
 
 	// 액션 메서드 시작
@@ -84,6 +90,19 @@ public class UsrArticleController {
 		deleteArticle(id);
 
 		return id + "번 게시물을 삭제했습니다.";
+	}
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+		Article article = getArticle(id);
+
+		if (article == null) {
+			return id + "번 게시물은 존재하지 않습니다.";
+		}
+
+		modifyArticle(id, title, body);
+
+		return id + "번 게시물을 수정했습니다";
 	}
 	// 액션메서드 끝
 }
