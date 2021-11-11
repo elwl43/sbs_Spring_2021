e932576a3d9b7fb4f7ede4415d19ae501edcac13
elwl43 <elwl43@gmail.com>
2021-11-11 17:13:05 +0900

    MemberRepository 추가, 회원 DB에 등록

--- src/main/java/com/sbs/exam/demo/service/MemberService.java
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -2,12 +2,18 @@ package com.sbs.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sbs.exam.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
+	private MemberRepository memberRepository;
 
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		// TODO Auto-generated method stub
-		
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 
 }
