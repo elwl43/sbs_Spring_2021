dc90f1db53d439ba27860f6f43e863d98e9b3a11
elwl43 <elwl43@naver.com>
2021-11-16 16:52:23 +0900

    article/doAdd에 ResultData 적용

--- src/main/java/com/sbs/exam/demo/service/ArticleService.java
+++ src/main/java/com/sbs/exam/demo/service/ArticleService.java
@@ -3,7 +3,9 @@ package com.sbs.exam.demo.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import com.sbs.exam.demo.repository.ArticleRepository;
+import com.sbs.exam.demo.util.Ut;
 import com.sbs.exam.demo.vo.Article;
+import com.sbs.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
@@ -13,19 +15,25 @@ public class ArticleService {
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id = articleRepository.getLastInsertId();
+
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);		
 	}
+	
 	public List<Article> getArticles() {
 		return articleRepository.getArticles();
 	}
+	
 	public Article getArticle(int id) {
 		return articleRepository.getArticle(id);
 	}
+	
 	public void deleteArticle(int id) {
 		articleRepository.deleteArticle(id);
 	}
+	
 	public void modifyArticle(int id, String title, String body) {
 		articleRepository.modifyArticle(id, title, body);
 	}
