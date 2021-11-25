6c939f3b5ad0b78cb225ec3a9320d2e2c216b53c
elwl43 <elwl43@naver.com>
2021-11-25 17:58:18 +0900

    Rq 객체를 인터셉터에서 한번만 만들기

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -15,6 +15,7 @@ import com.sbs.exam.demo.service.ArticleService;
 import com.sbs.exam.demo.util.Ut;
 import com.sbs.exam.demo.vo.Article;
 import com.sbs.exam.demo.vo.ResultData;
+import com.sbs.exam.demo.vo.Rq;
 
 @Controller
 public class UsrArticleController {
@@ -27,7 +28,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
 
-		Rq rq = new Rq(req);  
+		Rq rq = (Rq)req.getAttribute("rq");  
 
 		if (rq.isLogined() == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요");
@@ -53,7 +54,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/list")
 	public String showList(HttpServletRequest req, Model model) {
 
-		Rq rq = new Rq(req);  
+		Rq rq = (Rq)req.getAttribute("rq");
 
 		List<Article> articles = articleService.getArticles(rq.getLoginedMemberId());
 		
@@ -65,7 +66,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(HttpServletRequest req, Model model, int id) {
 
-		Rq rq = new Rq(req);  
+		Rq rq = (Rq)req.getAttribute("rq"); 
 
 		Article article = articleService.getArticle(rq.getLoginedMemberId(), id);
 
@@ -78,7 +79,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Article> getArticle(HttpServletRequest req, int id) {
 
-		Rq rq = new Rq(req);  
+		Rq rq = (Rq)req.getAttribute("rq"); 
 
 		Article article = articleService.getArticle(rq.getLoginedMemberId(), id);
 
@@ -92,7 +93,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public String doDelete(HttpServletRequest req, int id) {
 
-		Rq rq = new Rq(req);  
+		Rq rq = (Rq)req.getAttribute("rq");  
 
 		if (rq.isLogined() == false) {
 			return Ut.jsHistoryBack("로그인 후 이용해주세요");
@@ -115,7 +116,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
-		Rq rq = new Rq(req);  
+		Rq rq = (Rq)req.getAttribute("rq"); 
 
 		if (rq.isLogined() == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요");
