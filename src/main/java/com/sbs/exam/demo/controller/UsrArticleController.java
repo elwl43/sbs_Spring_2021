6619187bd3e743a771618e6aa0126543b9bad566
elwl43 <elwl43@naver.com>
2021-11-30 17:53:08 +0900

    게시물 리스트에서 존재하지 않는 게시판에 대한 처리historyBackOnView

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -59,10 +59,14 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/list")
 	public String showList(HttpServletRequest req, Model model, int boardId) {
+		Rq rq = (Rq) req.getAttribute("rq");
+		
 		Board board = boardService.getBoardById(boardId);
 
-		Rq rq = (Rq)req.getAttribute("rq");
-
+		if (board == null) {
+			return rq.historyBackOnView(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
+		}
+		
 		List<Article> articles = articleService.getArticles(rq.getLoginedMemberId());
 		
 		model.addAttribute("board", board);
